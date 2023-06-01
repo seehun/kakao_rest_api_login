a301d524c11d384c8e84b52226c8a57bcb141a6c
millifail <hwangbird99@gmail.com>
2023-06-01 21:19:47 +0900

    토큰받기 -> 토큰정보 토대로 유저정보 가져오기  과정 수행 비동기 함수들을 순서대로 실행하기 위해서 await 활용 ( then 삭제 ) useEffect하나에 담아서 코드 한번만 실행하도록 변경,  기존에는 유저정보를 무한정 가져왔음

--- src/component/LoginProcess.js
+++ src/component/LoginProcess.js
@@ -5,12 +5,14 @@ import { useEffect, useState } from 'react';
 
 function LoginProcess() {
   const [token, setToken] = useState(null);
+  const [userObj, setUserObj] = useState(null);
   const REST_API_KEY = '60af673ea132c4e08ec37a492dca87ab';
   const REDIRECT_URI = 'http://localhost:3000/loginProcess';
 
+  //url에 인가코드가 있음
   const code = new URLSearchParams(window.location.search).get('code');
 
-  const get_token = async () => {
+  useEffect(() => {
     const body = {
       grant_type: 'authorization_code',
       client_id: REST_API_KEY,
@@ -18,41 +20,28 @@ function LoginProcess() {
       code: code,
     };
 
-    const res = await axios
-      .post('https://kauth.kakao.com/oauth/token', body, {
+    const sequential = async () => {
+      //token 받기
+      const res = await axios.post(
+        'https://kauth.kakao.com/oauth/token',
+        body,
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        }
+      );
+      //사용자 정보 가져오기
+      const user_info = await axios.get('/v2/user/me', {
         headers: {
+          Authorization: `Bearer ${res.data.access_token}`,
           'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
         },
-      })
-      .then((res) => {
-        // console.log('token:', res);
-        // console.log('id_token: ', res.data.id_token);
-        // console.log('id_token_docode: ', jwt_decode(res.data.id_token));
-        setToken(res);
       });
-
-    //
-  };
-
-  const bring_user_info = async () => {
-    await axios
-      .get('/v2/user/me', {
-        headers: {
-          Authorization: `Bearer ${token.data.access_token}`,
-          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-        },
-      })
-      .then((res) => {
-        console.log('사용자 정보 가져오기 ', res);
-      });
-  };
-
-  useEffect(get_token, []);
-  if (token) {
-    // console.log(token);
-    // console.log('access token: ', token.data.access_token);
-    bring_user_info();
-  }
+      console.log(user_info);
+    };
+    sequential();
+  });
 
   return (
     <>
